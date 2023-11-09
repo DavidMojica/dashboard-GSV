68a678b12636454d0db5042ce5e3ddf0d194ff78
David Mojica <davidmojicav@gmail.com>
2023-11-09 09:19:04 -0500

    9-11-23 V

---
+++ processes/PDOconn.php
@@ -0,0 +1,11 @@
+<?php
+    $dsn      = "mysql:host=localhost;dbname=bd_movilidad;charset=UTF8";
+    $username = "root";
+    $password = "";
+    try{
+        $pdo = new PDO($dsn, $username, $password);
+        $pdo ->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    } catch(PDOException $e){
+        echo "Error de conexion = ". $e->getMessage();
+    }
+?>
---
+++ processes/login_process.php
@@ -0,0 +1,3 @@
+<?php
+    include("PDOconn.php");
+?>
