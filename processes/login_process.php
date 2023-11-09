47a51476e6e5953f253171164fa7106c4b709966
David Mojica <davidmojicav@gmail.com>
2023-11-09 09:33:11 -0500

    9-11-23 VI

--- processes/login_process.php
+++ processes/login_process.php
@@ -1,3 +1,37 @@
 <?php
     include("PDOconn.php");
+
+    if($_SERVER['REQUEST_METHOD'] == 'POST'){
+        #Variables provenientes de Ajax en login.js
+        $user      = trim($_POST['user']);
+        $pass      = trim($_POST['pass']);
+        #Variables de php
+        $errors = [];
+        $queryError = "error";
+        $ban = false;
+
+        #-----------Validaciones para cada campo--------------#
+        #Campos vacíos.
+        #Longitud.
+        #Confirmar concordancia
+
+        #Nombre
+        if(empty($user)){
+            $errors[] = "El campo de usuario es obligatorio. <br>";
+        }
+        else if(empty($pass)){
+            $errors[] = "El campo contraseña es obligatorio <br>";
+        }
+
+        if(empty($errors)){
+            $query = "SELECT * FROM tbl_usuario WHERE username = :username AND pass = :pass";
+            $stmt = $pdo->prepare($query);
+                $user = filter_input(INPUT_POST,"user", FILTER_SANITIZE_STRING);
+                $pass = filter_input(INPUT_POST,"pass", FILTER_SANITIZE_STRING);
+                $pass_hash = hash('sha256', $pass);
+
+
+
+        }
+    }
 ?>
