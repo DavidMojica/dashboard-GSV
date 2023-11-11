c5c46a6256a19c3cfca251b69ac5dee7a8e8fb7a
David Mojica <davidmojicav@gmail.com>
2023-11-11 07:32:25 -0500

    11-11-23 I

---
+++ initial_data.sql
@@ -0,0 +1,13 @@
+INSERT INTO `tbl_meses`(`nombre`) VALUES ('Enero');
+INSERT INTO `tbl_meses`(`nombre`) VALUES ('Febrero');
+INSERT INTO `tbl_meses`(`nombre`) VALUES ('Marzo');
+INSERT INTO `tbl_meses`(`nombre`) VALUES ('Abril');
+INSERT INTO `tbl_meses`(`nombre`) VALUES ('Mayo');
+INSERT INTO `tbl_meses`(`nombre`) VALUES ('Junio');
+INSERT INTO `tbl_meses`(`nombre`) VALUES ('Julio');
+INSERT INTO `tbl_meses`(`nombre`) VALUES ('Agosto');
+INSERT INTO `tbl_meses`(`nombre`) VALUES ('Septiembre');
+INSERT INTO `tbl_meses`(`nombre`) VALUES ('Octubre');
+INSERT INTO `tbl_meses`(`nombre`) VALUES ('Noviembre');
+INSERT INTO `tbl_meses`(`nombre`) VALUES ('Diciembre');
+
