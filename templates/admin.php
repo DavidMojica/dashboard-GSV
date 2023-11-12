2be0c619c3e6d66245a5358f7c2895e007da7e69
David Mojica <davidmojicav@gmail.com>
2023-11-12 11:42:59 -0500

    12-11-23 XI

--- templates/admin.php
+++ templates/admin.php
@@ -31,6 +31,7 @@ include('../processes/PDOconn.php');
     <link rel="stylesheet" href="styles/bg_dotted.css">
     <link rel="stylesheet" href="styles/btn_type_A.css">
     <link rel="stylesheet" href="styles/select_type_A.css">
+    <link rel="stylesheet" href="styles/in_bright.css">
     <script src="../scripts/admin.js" defer></script>
 
     <title>Admin</title>
@@ -52,105 +53,113 @@ include('../processes/PDOconn.php');
                             <input type="submit" value="Cerrar Sesión" class="btn_type_A">
                         </form>
                     </li>
-                    <li class="nav-item m-1">
-                        <button type="button" class="btn_type_A" id="toggle_dane">Población DANE</button>
-                    </li>
                 </ul>
             </div>
         </div>
     </nav>
 
     <main>
-        <div class="card" >
+        <div class="card">
             <div class="content" id="form_card">
                 <div class="back">
                     <div class="back-content">
                         <form action="" method="POST" id="formAccidentes">
-                            <div class="top_area">
-                                <h2>Accidentes</h2>
+                            <div class="top_1">
+                                <h2>Formulario de Accidentes</h2>
                             </div>
 
-                            <label for="municipio" aria-hidden="true">Municipio</label>
+                            <h3>¿Dónde?</h3>
                             <div class="select">
-                            <select name="municipio" id="municipio" class="select_A">
-                                <?php
-                                $query = "SELECT * FROM tbl_municipio";
-                                $stmt = $pdo->prepare($query);
-                                $stmt->execute();
-                                $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+                                <select name="municipio" id="municipio" class="select_A">
+                                    <?php
+                                    $query = "SELECT * FROM tbl_municipio";
+                                    $stmt = $pdo->prepare($query);
+                                    $stmt->execute();
+                                    $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
-                                foreach ($result as $row) {
-                                    echo "<option value='" . $row['id'] . "'>" . $row['nombre'] . "</option>";
-                                }
-                                ?>
-                            </select>
-
+                                    foreach ($result as $row) {
+                                        echo "<option value='" . $row['id'] . "'>" . $row['nombre'] . "</option>";
+                                    }
+                                    ?>
+                                </select>
                             </div>
 
-                            
-                            
-
-                            <label for="victima">Tipo de víctima</label>
+                            <h3>Tipo de víctima</h3>
                             <div class="select">
-                            <select name="victima" id="victima">
-                                <?php
-                                $query = "SELECT * FROM tbl_vehiculo";
-                                $stmt = $pdo->prepare($query);
-                                $stmt->execute();
-                                $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+                                <select name="victima" id="victima">
+                                    <?php
+                                    $query = "SELECT * FROM tbl_vehiculo";
+                                    $stmt = $pdo->prepare($query);
+                                    $stmt->execute();
+                                    $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
-                                foreach ($result as $row) {
-                                    echo "<option value='" . $row['id'] . "'>" . $row['nombre'] . "</option>";
-                                }
-                                ?>
-                            </select>
+                                    foreach ($result as $row) {
+                                        echo "<option value='" . $row['id'] . "'>" . $row['nombre'] . "</option>";
+                                    }
+                                    ?>
+                                </select>
                             </div>
 
-                            
-                            <label for="cantidad">cantidad</label>
-                            <input type="number" name="cantidad" id="cantidad" class="input">
+                            <div class=" mid_1">
+                                <div class="input-group">
+                                    <input required="" type="number" name="cantidad" min="0" id="cantidad" class="input" autocomplete="off">
+                                    <label class="user-label"><i class="fa fa-hashtag" aria-hidden="true"></i> de víctimas</label>
+                                </div>
 
-                            
-                            <label for="anio">Ingrese el año</label>
-                            <input type="number" class="input" id="anio" min="0" name="anio" placeholder="YYYY" pattern="\d{4}" title="Ingrese un formato válido (YYYY)">
+                                <div>
+                                    <label for="toggle">Muerte</label>
+                                    <label class="switch">
+                                        <input type="checkbox" id="toggle">
+                                        <span class="slider"></span>
+                                    </label>
+                                    <label for="toggle">Lesión</label>
+                                </div>
+                            </div>
 
-                            <label for="mes">Mes</label>
-                            <div class="select">
-                            <select name="mes" id="mes">
-                                <option value="1">Enero</option>
-                                <option value="2">Febrero</option>
-                                <option value="3">Marzo</option>
-                                <option value="4">Abril</option>
-                                <option value="5">Mayo</option>
-                                <option value="6">Junio</option>
-                                <option value="7">Julio</option>
-                                <option value="8">Agosto</option>
-                                <option value="9">Septiembre</option>
-                                <option value="10">Octubre</option>
-                                <option value="11">Noviembre</option>
-                                <option value="12">Diciembre</option>
-                            </select>
+
+                            <div class="mid_2">
+                                <h4>¿Cuándo?</h4>
+
+                                <div class="input-group">
+                                    <input required="" type="number" name="cantidad" id="cantidad" class="input" autocomplete="off">
+                                    <label class="user-label">Año (YYYY)</label>
+                                </div>
+
+                                <div class="select">
+                                    <select name="mes" id="mes">
+                                        <option value="1">Enero</option>
+                                        <option value="2">Febrero</option>
+                                        <option value="3">Marzo</option>
+                                        <option value="4">Abril</option>
+                                        <option value="5">Mayo</option>
+                                        <option value="6">Junio</option>
+                                        <option value="7">Julio</option>
+                                        <option value="8">Agosto</option>
+                                        <option value="9">Septiembre</option>
+                                        <option value="10">Octubre</option>
+                                        <option value="11">Noviembre</option>
+                                        <option value="12">Diciembre</option>
+                                    </select>
+                                </div>
                             </div>
 
                             <div class="bot_1">
-                            <p id="msg_error" class="msg_error"></p>
-                            </div>
-                            
-                            <div class="bot_2">
-                                <label for="toggle">Muerte</label>
-                                <label class="switch">
-                                    <input type="checkbox" id="toggle">
-                                    <span class="slider"></span>
-                                </label>
-                                <label for="toggle">Lesión</label>
+                                <p id="msg_error" class="msg_error"></p>
                             </div>
 
-                            <div class="bot_3">
+                            <div>
+                                <button type="button" class="btn_type_A" id="toggle_dane">Formulario DANE</button>
+                            </div>
+
+                            <div>
                                 <button class="button">Ingresar</button>
                             </div>
+                            
                         </form>
                     </div>
                 </div>
+
+
 
 
                 <div class="front">
