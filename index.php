0145bcb054b17d4398950ffd3904610050f6de59
David Mojica <davidmojicav@gmail.com>
2023-11-13 13:05:34 -0500

    13-11-23 VII

--- index.php
+++ index.php
@@ -13,6 +13,7 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 
     <link rel="stylesheet" href="templates/styles/btn_type_A.css">
+    <link rel="stylesheet" href="templates/styles/btn_light.css">
     <link rel="stylesheet" href="templates/styles/base.css">
     <link rel="stylesheet" href="templates/styles/index.css">
     <title>Dashboard</title>
@@ -24,36 +25,41 @@
             <img src="https://www.antioquia.gov.co/images/PDF2/Comunicaciones/imagen-de-marca/logo.svg" alt="GOV.CO" class="gov_logo">
         </div>
         <nav class="navbar navbar-expand-lg navbar_d2">
-            <a href="https://antioquia.gov.co/" class="ans_logo">
-                <img src="https://www.mintransporte.gov.co/info/mintransporte/media/pubInt/thumbs/thpub_700x400_10745.jpg" alt="Logo Gobernacion Antioquia" class="ans_logo">
-            </a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarNav">
-                <ul class="navbar-nav ms-auto">
-                    <?php
-                    session_start();
-                    // Comprueba si la sesión está iniciada
-                    if (!isset($_SESSION['username'])) {
-                    ?>
-                        <li class="nav-item m-1">
-                            <a href="templates/login.php">
-                                <button class="btn_type_A">Iniciar Sesión <i class="fa fa-sign-in" aria-hidden="true"></i></button>
-                            </a>
-                        </li>
+            <div class="container">
+                <a href="https://antioquia.gov.co/" class="ans_logo">
+                    <img src="https://www.mintransporte.gov.co/info/mintransporte/media/pubInt/thumbs/thpub_700x400_10745.jpg" alt="Logo Gobernacion Antioquia" class="ans_logo">
+                </a>
 
-                    <?php
-                    } else {
-                    ?>
-                        <li class="nav-item m-1"><a href="templates/admin.php"><button class="btn_type_A">Administración</button></a></li>
-                        <li class="nav-item m-1">
-                            <form action="processes/logout.php" method="post">
-                                <button class="btn_type_A">Cerrar Sesión</button>
-                            </form>
-                        </li>
-                    <?php } ?>
-                </ul>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNav">
+                    <ul class="navbar-nav ms-auto">
+                        <?php
+                        session_start();
+
+                        // Comprueba si la sesión está iniciada
+                        if (!isset($_SESSION['username'])) {
+                        ?>
+                            <li class="nav-item m-1">
+                                <a href="templates/login.php">
+                                    <button class="button-light">Iniciar Sesión   <i class="fa fa-sign-in" aria-hidden="true"></i></button>
+                                </a>
+                            </li>
+
+                        <?php
+                        } else {
+                        ?>
+                            <li class="nav-item m-1"><a href="templates/admin.php"><button class="button-light">Administración</button></a></li>
+                            <li class="nav-item m-1">
+                                <form action="processes/logout.php" method="post">
+                                    <button class="button-light">Cerrar Sesión</button>
+                                </form>
+                            </li>
+
+                        <?php } ?>
+                    </ul>
+                </div>
             </div>
         </nav>
     </div>
