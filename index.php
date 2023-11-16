dca728710f55dfab53d5cd4b5fde0de1f8b26f97
David Mojica <davidmojicav@gmail.com>
2023-11-16 10:22:09 -0500

    16-11-23 V

--- index.php
+++ index.php
@@ -227,7 +227,7 @@ include("processes/PDOconn.php");
                     <div class="row">
                         <div class="col-5"><img alt="icono colombia oficial" class="img-responsive " id="govco" src="https://www.mintransporte.gov.co/info/mintransporte/media/bloque2387.png"></div>
 
-                        <div class="col-7">
+                        <div class="col-3">
 
                             <a class="section enl-fb" rel="noreferrer" target="_blank" href="https://www.facebook.com/agencianacionaldeseguridadvial/?fref=ts" title="Enlace a Facebook"><svg class="svg-inline--fa fa-facebook-f fa-w-10" style="width: 100%;" aria-hidden="true" focusable="false" data-prefix="fab" data-icon="facebook-f" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512" data-fa-i2svg="">
                                     <path fill="currentColor" d="M279.14 288l14.22-92.66h-88.91v-60.13c0-25.35 12.42-50.06 52.24-50.06h40.42V6.26S260.43 0 225.36 0c-73.22 0-121.08 44.38-121.08 124.72v70.62H22.89V288h81.39v224h100.17V288z"></path>
